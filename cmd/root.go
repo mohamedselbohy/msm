f91fdcd383d1a4ed83181bf1c65a62f092bde160
mohamedselbohy <msayedelbohy@hotmail.com>
2025-11-16 15:58:57 +0200

    Added dynamic autocompletions

--- cmd/root.go
+++ cmd/root.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/mohamedselbohy/msm/internal/docker"
 	"github.com/spf13/cobra"
 )
 
@@ -13,12 +15,32 @@ var rootCmd = &cobra.Command{
 	Long:  `A ROS interface, to deal with docker-based ros environments`,
 }
 
+func CompleteWorkspaces(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	cli, ctx, err := docker.GetClient()
+	if err != nil {
+		return nil, cobra.ShellCompDirectiveError
+	}
+	workspaces, err := docker.ListRunningContainers(cli, ctx)
+	if err != nil {
+		return nil, cobra.ShellCompDirectiveError
+	}
+	var completions []string
+	for _, ws := range workspaces {
+		if strings.HasPrefix(ws, toComplete) {
+			completions = append(completions, ws)
+		}
+	}
+	return completions, cobra.ShellCompDirectiveDefault
+}
+
 func init() {
 	rootCmd.AddCommand(initCmd)
 	rootCmd.AddCommand(pkgCmd)
 	rootCmd.AddCommand(execCmd)
 	rootCmd.AddCommand(listCmd)
 	rootCmd.AddCommand(removeCmd)
+	execCmd.ValidArgsFunction = CompleteWorkspaces
+	removeCmd.ValidArgsFunction = CompleteWorkspaces
 }
 
 func Execute() {
