243d47c2189375a4864cd8fabd199675e94f936a
mohamedselbohy <msayedelbohy@hotmail.com>
2025-11-26 14:42:03 +0200

    Feat: Colorize status when listing workspaces

--- cmd/list.go
+++ cmd/list.go
@@ -8,6 +8,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func colorStatus(status string, workspace string) string {
+	switch status {
+	case "active":
+		return "\033[32m" + "(" + status + ")" + "\033[0m"
+	case "inactive":
+		return "\033[90m" + "(" + status + ")\033[0m" + "\033[90m: activate with (msm activate " + workspace + ")\033[0m"
+	case "irresponsive":
+		return "\033[31m" + "(" + status + ")\033[0m" + "\033[90m: Revive with (msm revive " + workspace + ")\033[0m"
+	default:
+		return status
+	}
+}
+
 var listCmd = &cobra.Command{
 	Use:   "ls",
 	Short: "Lists open workspaces",
@@ -28,7 +41,7 @@ var listCmd = &cobra.Command{
 			return
 		}
 		for i, ws := range workspaces {
-			fmt.Println(strconv.Itoa(i+1) + ": " + ws + "\t(" + docker.IsActive(cli, ctx, "ros-"+ws) + ")")
+			fmt.Println(strconv.Itoa(i+1) + ": " + ws + "\t" + colorStatus(docker.IsActive(cli, ctx, "ros-"+ws), ws))
 		}
 	},
 }
